25dc5edeff0b03cb5ead51f765c330e6c1cfa369
Sivaperuman K <SivaperumanK@CADSIndia.com>
2018-08-03 20:03:59 +0530

    Added Heros component

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero'
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+
+  hero:Hero={
+    id:1,
+    name:"Sivaperuman"
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
